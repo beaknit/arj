41f7f489b9696ad77ef30cc6df2bb7937717a395
beaknit <beaknit@gmail.com>
2017-09-30 15:26:42 -0400

    Add get-payload

--- src/arj/core.clj
+++ src/arj/core.clj
@@ -12,13 +12,16 @@
 (defn my-strip [s]
   (clojure.string/replace s "\"" ""))
 
+(defn get-payload [q]
+  (:body (get (:messages (receive-message q)) 0)))
+
 (defn -main [& args]
   (let [x (:seed (:options (parse-opts args cli-options)))]
     (println "Input:" x)
     (create-queue x)
     (let [q (find-queue x)]
       (send-message q x)
-      (let [m (:body (get (:messages (receive-message q)) 0))]
+      (let [m (get-payload q)]
         (println "Output:" (my-strip m)))))
 ;;    (pprint (:queue-urls(list-queues))))
 )
